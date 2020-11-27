aa5e7a8d9a4ac990eb89ad00ec0a5bac074cd147
Nikita Akimov <korchiy@yandex.ru>
2020-11-27 15:31:49 +0300

    update

--- carousel.py
+++ carousel.py
@@ -4,12 +4,10 @@
 # GitHub
 #    https://github.com/Korchy/blender_carousel
 
-from mathutils import Vector
+from .object import Object
 
 
 class Carousel:
-
-    # _tmp_obj = []
 
     @classmethod
     def turntable(cls, context, scene_data, selection):
@@ -28,44 +26,20 @@ class Carousel:
             context=context,
             scene_data=scene_data
         )
-        # camera target
-        target_co = cls._geometry_center(
+        # bounding sphere
+        b_sphere_co, b_sphere_radius = Object.bounding_sphere(
             objects=selection,
-            count_mode=context.preferences.addons[__package__].preferences.center_count_mode
+            mode=context.preferences.addons[__package__].preferences.center_count_mode
         )
-        if target_co:
-            camera_target = scene_data.objects.new(name='camera_target', object_data=None)
-            camera_target.location = target_co
-            camera_target.empty_display_type = 'SPHERE'
-            turntable_collection.objects.link(object=camera_target)
+        if not b_sphere_co:
+            return
+        camera_target = scene_data.objects.new(name='camera_target', object_data=None)
+        camera_target.empty_display_type = 'SPHERE'
+        camera_target.location = b_sphere_co
+        camera_target.empty_display_size = b_sphere_radius
+        turntable_collection.objects.link(object=camera_target)
         # camera
-
-    @staticmethod
-    def _geometry_center(objects: list, count_mode='BBOX'):
-        # geometry center of objects from list
-        points_co_global = []
-        if count_mode == 'GEOMETRY':
-            # GEOMETRY - by all vertices/points
-            for obj in objects:
-                if obj.type == 'MESH':
-                    points_co_global.extend([obj.matrix_world @ vertex.co for vertex in obj.data.vertices])
-                if obj.type == 'CURVE':
-                    for spline in obj.data.splines:
-                        points_co_global.extend([obj.matrix_world @ point.co for point in spline.bezier_points])
-                else:
-                    # cant get all points - use bbox
-                    points_co_global.extend([obj.matrix_world @ Vector(bbox) for bbox in obj.bound_box])
-        elif count_mode == 'BBOX':
-            # BBOX - by object bounding boxes
-            for obj in objects:
-                points_co_global.extend([obj.matrix_world @ Vector(bbox) for bbox in obj.bound_box])
-
-        def get_center(l):
-            return (max(l) + min(l)) / 2 if l else 0.0
-
-        x, y, z = [[point_co[i] for point_co in points_co_global] for i in range(3)]
-        objects_center_global = [get_center(axis) for axis in [x, y, z]] if (x and y and z) else None
-        return objects_center_global
+        
 
     @classmethod
     def _turntable_collection(cls, context, scene_data):
---
+++ object.py
@@ -0,0 +1,40 @@
+# Nikita Akimov
+# interplanety@interplanety.org
+#
+# GitHub
+#    https://github.com/Korchy/blender_carousel
+
+from mathutils import Vector
+
+
+class Object:
+
+    @staticmethod
+    def bounding_sphere(objects, mode='BBOX'):
+        # return the bounding sphere center and radius for objects (in global coordinates)
+        if not isinstance(objects, list):
+            objects = [objects]
+        points_co_global = []
+        if mode == 'GEOMETRY':
+            # GEOMETRY - by all vertices/points - more precis, more slow
+            for obj in objects:
+                if obj.type == 'MESH':
+                    points_co_global.extend([obj.matrix_world @ vertex.co for vertex in obj.data.vertices])
+                elif obj.type == 'CURVE':
+                    for spline in obj.data.splines:
+                        points_co_global.extend([obj.matrix_world @ point.co for point in spline.bezier_points])
+                else:
+                    # cant get all points - use bbox
+                    points_co_global.extend([obj.matrix_world @ Vector(bbox) for bbox in obj.bound_box])
+        elif mode == 'BBOX':
+            # BBOX - by object bounding boxes - less precis, quick
+            for obj in objects:
+                points_co_global.extend([obj.matrix_world @ Vector(bbox) for bbox in obj.bound_box])
+
+        def get_center(l):
+            return (max(l) + min(l)) / 2 if l else 0.0
+
+        x, y, z = [[point_co[i] for point_co in points_co_global] for i in range(3)]
+        b_sphere_center = Vector([get_center(axis) for axis in [x, y, z]]) if (x and y and z) else None
+        b_sphere_radius = max(((point - b_sphere_center) for point in points_co_global)) if b_sphere_center else None
+        return b_sphere_center, b_sphere_radius.length
